9b9edfad796bf4e410ca739766165167f02c84b0
Alexis <alex.p.flores17@gmail.com>
2021-02-09 21:16:52 -0600

    Maquetacion

--- src/componentes/Servicios.js
+++ src/componentes/Servicios.js
@@ -3,8 +3,34 @@ import React from 'react';
 export const Servicios = () =>{
 
     return(
-        <div>
-
+        <div className="serviciosMain">
+            <div className="serviciosTitulo">Servicios</div>
+            <div className="serviciosContenido">
+                <div className="serviciosItem" >d</div>
+                <div className="serviciosItem" >d</div>
+                <div className="serviciosItem" >d</div>
+                <div className="serviciosItem" >d</div>
+                <div className="serviciosItem" >d</div>
+                <div className="serviciosItem" >d</div>
+                <div className="serviciosItem" >d</div>
+            </div>
+            <div className="serviciosQuote">
+                <div className="serviciosQuoteItem">
+                    <div>
+                        <div>"</div>
+                        <div>
+                            Lo mejor en contabilidad con expertos especializados...
+                            HGR consultores son verdaderos profesionales.
+                        </div>
+                    </div>
+                    <div>
+                        Alexis Flores
+                    </div>
+                    <div>
+                        CTO, Light and Sound Disruptive Lab
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
