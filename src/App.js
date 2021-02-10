eb8662f531153faa631a33059f538af0e3c6ed77
Alexis <alex.p.flores17@gmail.com>
2021-02-09 20:07:26 -0600

    Inicio Pagina Garin

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import {Header}     from "./componentes/Header";
+import {Nosotros}   from "./componentes/Nosotros";
+import {Servicios}  from "./componentes/Servicios";
+import {Proyectos}  from "./componentes/Proyectos";
+import {Contacto}   from "./componentes/Contacto";
+import {Footer}     from "./componentes/Footer";
+
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <Nosotros />
+      <Servicios />
+      <Proyectos />
+      <Contacto />
+      <Footer />
     </div>
   );
 }
