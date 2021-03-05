5991bfe5c09a8ad9c3a01dec31749b723ca1e92e
Alexis <alex.p.flores17@gmail.com>
2021-03-04 22:43:38 -0600

    Revert "update"
    This reverts commit b4104cbef9570d955b25cdeeaecec4f741be1d20, reversing
    changes made to 7210e2890e8d30806cd557a6e217860c60875986.

--- src/componentes/AvisoDePrivacidad.js
+++ src/componentes/AvisoDePrivacidad.js
@@ -1,26 +1,9 @@
-import React from 'react';
-import { MdClose } from "react-icons/md";
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react'
 
 export const AvisoDePrivacidad = () => {
-
-    const dispatch = useDispatch();
-    const state = useSelector(state => state.contactoReducer)
-
-    const cerrar = () => {
-        console.log(cerrar);
-    }
-
     return (
-        <div className="avisoDePrivacidadContainer">
-            <div className="btn-cerrar">
-                <span onClick={() => cerrar()} className="cerrar-icon">
-                    <MdClose />
-                </span>
-            </div>
-            <div className="contacto-content">
-                <h1>Aviso de privacidad</h1>
-            </div>
+        <div>
+            Aviso de privacidad
         </div>
     )
 }
