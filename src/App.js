7210e2890e8d30806cd557a6e217860c60875986
Alexis <alex.p.flores17@gmail.com>
2021-03-04 22:30:29 -0600

    Aviso de privacidad

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {Proyectos}  from "./componentes/Proyectos";
 import {Contacto}   from "./componentes/Contacto";
 import {Footer}     from "./componentes/Footer";
 import { Inicio } from "./componentes/Inicio";
+import { AvisoDePrivacidad } from "./componentes/AvisoDePrivacidad";
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
     <div className="App">
       <Header />
       <Inicio />
+      <AvisoDePrivacidad />
       <Nosotros />
       <Servicios />
       <Proyectos />
---
+++ src/componentes/AvisoDePrivacidad.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const AvisoDePrivacidad = () => {
+    return (
+        <div>
+            Aviso de privacidad
+        </div>
+    )
+}
